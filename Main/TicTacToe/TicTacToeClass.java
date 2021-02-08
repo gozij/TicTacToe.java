0d8f630536aebe8e0afdd8d61e00f70cae839727
Ngozi Mbabie <gozij@yahoo.com>
2021-02-08 12:35:12 -0600

    First commit

---
+++ Main/TicTacToe/TicTacToeClass.java
@@ -0,0 +1,35 @@
+package Main.TicTacToe;
+
+public class TicTacToeClass {
+
+	public boolean isFull() {
+		return false;
+	}
+
+	public boolean isWinner(char c) {
+		return false;
+	}
+
+	public void displayBoard() {
+	}
+
+	public boolean isValid(int r, int c) {
+		return false;
+	}
+
+	public char playerAt(int r, int c) {
+		return 0;
+	}
+
+	public boolean isCat() {
+		return false;
+	}
+
+	public void playMove(char p, int r, int c) {
+	}
+
+	public int numTurns() {
+		return 0;
+	}
+
+}
